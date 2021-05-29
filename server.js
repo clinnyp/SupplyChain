d2771a9d54db69b75d8c4090e703c724cfdff06c
thefattestcat <48980192+thefattestcat@users.noreply.github.com>
2021-05-29 14:10:47 +1200

    balance endpoint

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+const { Api } = require('@cennznet/api');
 const express = require('express')
-const app = express()
- 
+const app = express();
+const getBalance = require('./util/getBalance')
+
+const NIKAU_WS = 'wss://nikau.centrality.me/public/ws';
+const PETER = '5GWVMKzwKVhdUXAv9dgTUZ4XUxXXTixgFZHnvKHRfwK93Hdn';
+const CENNZ = 16000;
+
+
+
 app.get('/', function (req, res) {
-    let test = {"data": "this is a test object", "array": [0,1,2]}
+    let test = { "data": "this is a test object", "array": [0, 1, 2] }
     res.send(test)
 })
 
- 
+app.get('/admin/balance', async (req, res) => {
+    const api = await Api.create({ provider: NIKAU_WS });
+    let CENN_balance =  await api.query.genericAsset.freeBalance(16000, PETER);
+    //let CPAY_balance =  await api.query.genericAsset.freeBalance(16001, PETER);
+    //console.log(CENN_balance, CPAY_balance)
+    res.send({CENNZ: CENN_balance, CPAY: 0});
+})
+
+
 const PORT = 7000 || process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
-  })
+})
