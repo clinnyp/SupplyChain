7559064398413a96353a59bb178c22b3fff995ae
thefattestcat <48980192+thefattestcat@users.noreply.github.com>
2021-05-30 09:39:11 +1200

    fix

--- client/pages/admin/dashboard.js
+++ client/pages/admin/dashboard.js
@@ -94,7 +94,7 @@ function Dashboard() {
                   <Tasks
                     checkedIndexes={[0, 3]}
                     tasksIndexes={[0, 1, 2, 3, 4, 5, 6, 7]}
-                    tasks={delegatorAddr}
+                    tasks={[]}
                   />
                 ),
               },
