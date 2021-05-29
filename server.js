d7eabb619ed02739d744e5e0ce2b0ef6c1e8fb85
thefattestcat <48980192+thefattestcat@users.noreply.github.com>
2021-05-29 18:15:47 +1200

    endpoint query

--- server.js
+++ server.js
@@ -1,29 +1,49 @@
 const { Api } = require('@cennznet/api');
 const express = require('express')
-var cors = require('cors')
+const cors = require('cors');
 const app = express();
 
 const NIKAU_WS = 'wss://nikau.centrality.me/public/ws';
 const PETER = '5GWVMKzwKVhdUXAv9dgTUZ4XUxXXTixgFZHnvKHRfwK93Hdn';
+const FONTERRA = '5GhH2czRJFktx6mtLjj7jcD3fJPCHB3ofo3PMKAT7xzSRso2';
 const CENNZ = 16000;
 
-app.use(cors())
+async function _initialize(api) {
+    let data = {}
+    data.stakingAssetId = await api.query.genericAsset.stakingAssetId(); // 1 on MainNet
+    data.spendingAssetId = await api.query.genericAsset.spendingAssetId(); // 2 on MainNet
+    data.CENNZ = await api.query.genericAsset.freeBalance(16000, FONTERRA);
+    data.CPAY = await api.query.genericAsset.freeBalance(16001, FONTERRA);
+    data.locks = await api.query.genericAsset.locks(FONTERRA);
+    data.asset = await api.rpc.genericAsset.registeredAssets();
+    data.timestamp = Date.now();
 
-app.get('/', function (req, res) {
-    let test = { "data": "this is a test object", "array": [0, 1, 2] }
-    res.send(test)
-})
+    return data;
+}
 
-app.get('/admin/balance', async (req, res) => {
+async function main() {
     const api = await Api.create({ provider: NIKAU_WS });
-    let CENN_balance =  await api.query.genericAsset.freeBalance(16000, PETER);
-    //let CPAY_balance =  await api.query.genericAsset.freeBalance(16001, PETER);
-    //console.log(CENN_balance, CPAY_balance)
-    res.send({CENNZ: CENN_balance, CPAY: 0});
-})
+    const init_data = await _initialize(api);
+
+    app.use(cors());
+
+    app.get('/', function (req, res) {
+        res.send(await _initialize(api))
+    })
+
+    app.get('/admin/balance', async (req, res) => {
+        res.send(await _initialize(api));
+    })
+
+    const PORT = 7000 || process.env.PORT;
+    app.listen(PORT, () => {
+        console.log(`Example app listening at http://localhost:${PORT}`)
+    })
+}
+
+main().then(() => { }).catch((err) => {
+    console.log(`Main Error: ${err}`)
+    exit(1)
+});
 
 
-const PORT = 7000 || process.env.PORT;
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
-})
